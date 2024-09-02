377cf38560811f7130199c1601999d202151a8ce
Winston Thai <winstonthai1@gmail.com>
2024-09-02 11:48:55 -0600

    initial commit for frontend

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import { Container } from "@mui/material";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Container>test</Container>
+      </div>
+    </>
+  );
+}
+
+export default App;
