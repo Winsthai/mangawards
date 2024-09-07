3eaffa15fd5143dc9e231f5242099493d99136c2
Winston Thai <winstonthai1@gmail.com>
2024-09-07 02:32:08 -0600

    added filtering by tags

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -23,17 +23,6 @@ export default tseslint.config(
         "warn",
         { allowConstantExport: true },
       ],
-      "@stylistic/semi": "error",
-      "@typescript-eslint/no-unsafe-assignment": "error",
-      "@typescript-eslint/no-explicit-any": "error",
-      "@typescript-eslint/explicit-function-return-type": "off",
-      "@typescript-eslint/explicit-module-boundary-types": "off",
-      "@typescript-eslint/restrict-template-expressions": "off",
-      "@typescript-eslint/restrict-plus-operands": "off",
-      "@typescript-eslint/no-unused-vars": [
-        "error",
-        { argsIgnorePattern: "^_" },
-      ],
     },
   }
 );
--- client/src/components/Manga/MangaEntries.tsx
+++ client/src/components/Manga/MangaEntries.tsx
@@ -1,7 +1,20 @@
-import { Pagination } from "@mui/material";
+import {
+  Box,
+  Checkbox,
+  Chip,
+  Container,
+  ListItemText,
+  MenuItem,
+  Pagination,
+  Select,
+  SelectChangeEvent,
+  Stack,
+  Typography,
+} from "@mui/material";
 import { BasicManga } from "../../types";
 import MangaCard from "./MangaCard";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import { TAGS } from "../../constants";
 
 const NUMENTRIES = 50;
 
@@ -9,16 +22,72 @@ const MangaEntries = ({ manga }: { manga: BasicManga[] }) => {
   const [currManga, setCurrManga] = useState<BasicManga[]>([]);
   const [page, setPage] = useState(1);
 
+  /* Filtering */
+  const [tags, setTags] = useState<string[]>([]);
+
   useEffect(() => {
-    setCurrManga(manga.sort((a, b) => b.awards.length - a.awards.length));
-  });
+    const sortedManga = manga.sort((a, b) => b.awards.length - a.awards.length);
+
+    if (tags.length !== 0) {
+      setCurrManga(
+        sortedManga.filter((manga) =>
+          tags.every((tag) => manga.tags.includes(tag))
+        )
+      );
+    } else {
+      setCurrManga(sortedManga);
+    }
+  }, [tags]);
 
   const changePage = (_event: React.ChangeEvent<unknown>, value: number) => {
     setPage(value);
   };
 
+  const handleTagFilter = (event: SelectChangeEvent<string[]>) => {
+    setTags(event.target.value);
+  };
+
   return (
     <div>
+      <Container sx={{ display: "flex", marginTop: "2%" }}>
+        {/* Tags filtering */}
+        <Stack>
+          <Typography sx={{ marginBottom: "10px" }}>Tags</Typography>
+          <Select
+            multiple
+            value={tags}
+            onChange={handleTagFilter}
+            renderValue={(selected) => {
+              if (selected.length === 0) {
+                return <em>Any</em>;
+              }
+              return (
+                <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
+                  {selected.map((value) => (
+                    <Chip
+                      key={value}
+                      label={value}
+                      sx={{ backgroundColor: "#1976d2", color: "white" }}
+                    />
+                  ))}
+                </Box>
+              );
+            }}
+            sx={{
+              backgroundColor: "#1c1f26",
+              color: "white",
+            }}
+            displayEmpty
+          >
+            {TAGS.map((tag) => (
+              <MenuItem key={tag} value={tag}>
+                <Checkbox checked={tags.indexOf(tag) > -1} />
+                <ListItemText primary={tag} />
+              </MenuItem>
+            ))}
+          </Select>
+        </Stack>
+      </Container>
       {currManga.slice((page - 1) * 50, page * 50).map((individualManga) => (
         <MangaCard
           key={individualManga.title}
